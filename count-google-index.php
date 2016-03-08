2b7f8905951f3a96099b78da6acdd387ba0ef5c1
Ferri Sutanto <greenhouseprod@gmail.com>
2016-03-09 00:45:05 +0700

    Add validation url

--- count-google-index.php
+++ count-google-index.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+/*
  * Really really simple function return response as json
  * Ferri Sutanto
  * 25 Apr 2015 12:01PM.
@@ -18,11 +18,40 @@ if (! function_exists('responseJson')) {
     }
 }
 
+/*
+ * Taken from http://www.binarytides.com/validate-domain-name-filter_var-function-php/
+ */
+function filter_var_domain($domain)
+{
+    if (stripos($domain, 'http://') === 0) {
+        $domain = substr($domain, 7);
+    }
+
+    ///Not even a single . this will eliminate things like abcd, since http://abcd is reported valid
+    if (! substr_count($domain, '.')) {
+        return false;
+    }
+
+    if (stripos($domain, 'www.') === 0) {
+        $domain = substr($domain, 4);
+    }
+
+    $again = 'http://'.$domain;
+
+    return filter_var($again, FILTER_VALIDATE_URL);
+}
+
 $url = isset($_GET['url']) ? $_GET['url'] : null;
 
 if (! $url) {
     return responseJson([
         'error' => 'URL is required',
+    ], 403);
+}
+
+if (! filter_var_domain($url)) {
+    return responseJson([
+        'error' => 'URL is not valid',
     ], 403);
 }
 
